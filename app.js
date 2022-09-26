b43a9fba5bb86f5ae0a1784dbad573ddb8dff780
JuliethPaolaOlarte <julietholarte16@gmail.com>
2022-09-26 07:21:47 -0500

    cambios5

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get("/", function(req, res){
     res.sendFile(path.resolve(__dirname, "./src/views/home.html"))
 });
 
+app.get("/Ingresá ", function(req, res){
+    res.sendFile(path.resolve(__dirname, "src/views/login.html"))
+});
+
+app.get("/Creá tú cuenta", function(req, res){
+    res.sendFile(path.resolve(__dirname, "src/views/register.html"))
+});
+
 app.listen(process.env.PORT || 3000,() => (
     console.log("Servidor corriendo")))
 
